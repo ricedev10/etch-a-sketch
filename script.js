da4fb1f0ec18b7c8fea402e5498a1f4f25e3249b
Kai Hong <ricedev10@gmail.com>
2025-10-30 17:12:41 -0400

    remove width/height to grid size and add pixel density

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
+"use strict";
 const body = document.querySelector("body");
+const gridSize = document.querySelector("#grid-size");
+
+const pixelDensityInput = document.querySelector(
+	".pixel-density"
+);
+
+let pixelDensity = +pixelDensityInput.value;
+let before = pixelDensityInput.value;
+pixelDensityInput.addEventListener("focus", (event) => {
+	before = pixelDensityInput.value;
+});
+pixelDensityInput.addEventListener(
+	"beforeinput",
+	(event) => {
+		let num = +event.data;
+		if (isNaN(num)) {
+			event.preventDefault();
+		}
+	}
+);
+pixelDensityInput.addEventListener("keydown", (event) => {
+	if (event.key == "Enter") {
+		pixelDensityInput.blur();
+		const newValue = +pixelDensityInput.value;
+		if (newValue > 100) {
+			pixelDensityInput.value = before;
+		} else {
+			pixelDensity = newValue;
+			updateGrid();
+		}
+	}
+});
 
 // create a grid of 16x16 cells
 const container = document.createElement("div");
@@ -6,11 +39,32 @@ container.style.display = "flex";
 container.style.flexDirection = "column";
 body.appendChild(container);
 
-const grid = createGrid(16, 16, 16);
-grid.style.alignSelf = "center";
+let grid = createGrid(+gridSize.value, pixelDensity);
 container.appendChild(grid);
 
+// configuration of grid
+gridSize.addEventListener("focusout", updateGrid);
+gridSize.addEventListener("input", (event) =>
+	console.log(event.key)
+);
+
 //
+function updateGrid() {
+	const newGridSize = +gridSize.value;
+
+	if (
+		typeof newGridSize != "number" &&
+		typeof newGridSize != "number"
+	) {
+		return;
+	}
+
+	// remove current grid, and create a new one based on settings
+	grid.remove();
+	grid = createGrid(newGridSize, pixelDensity);
+	container.appendChild(grid);
+}
+
 function createCell(cellSize) {
 	const cell = document.createElement("div");
 	cell.style.backgroundColor = "gray";
@@ -37,17 +91,21 @@ function createCell(cellSize) {
 	return cell;
 }
 
-function createGrid(height, width, cellSize) {
+function createGrid(size, density) {
 	let grid = document.createElement("div");
+	grid.style.alignSelf = "center";
 	grid.style.display = "flex";
 	grid.style.flexWrap = "wrap";
 	grid.style.justifyContent = "flex-start";
 	grid.style.alignContent = "flex-start";
-	grid.style.width = width * cellSize + "px";
-	grid.style.height = height * cellSize + "px";
+	grid.style.width = size + "px";
+	grid.style.height = size + "px";
 
-	for (let x = 0; x < height; x++) {
-		for (let y = 0; y < width; y++) {
+	const cellSize = size / density;
+	size /= cellSize;
+
+	for (let x = 0; x < size; x++) {
+		for (let y = 0; y < size; y++) {
 			const cell = createCell(cellSize);
 			cell.setAttribute("class", "cell");
 			grid.appendChild(cell);
