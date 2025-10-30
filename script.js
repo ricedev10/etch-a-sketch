e85fe0f4b506448f7e88f3b2bab03e6a3fbad328
Kai Hong <ricedev10@gmail.com>
2025-10-30 14:04:11 -0400

    center grid, and cells highlight on hover

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
-const grid = document.querySelector(".grid");
+const body = document.querySelector("body");
 
 // create a grid of 16x16 cells
-createGrid(16, 16, 32);
+const container = document.createElement("div");
+container.style.display = "flex";
+container.style.flexDirection = "column";
+body.appendChild(container);
+
+const grid = createGrid(16, 16, 16);
+grid.style.alignSelf = "center";
+container.appendChild(grid);
 
 //
 function createCell(cellSize) {
 	const cell = document.createElement("div");
 	cell.style.backgroundColor = "gray";
 	cell.style.borderStyle = "solid";
+	cell.style.borderWidth = "0px";
 	cell.style.width = cellSize + "px";
 	cell.style.height = cellSize + "px";
+
+	// add hovering effect to cell
+	{
+		let cooldown = 0;
+		cell.addEventListener("mouseenter", () => {
+			cell.style.backgroundColor = "red";
+		});
+		cell.addEventListener("mouseleave", () => {
+			cooldown += 0.2;
+			setTimeout(
+				() => (cell.style.backgroundColor = "gray"),
+				cooldown * 1000
+			);
+		});
+	}
 
 	return cell;
 }
@@ -26,9 +49,10 @@ function createGrid(height, width, cellSize) {
 	for (let x = 0; x < height; x++) {
 		for (let y = 0; y < width; y++) {
 			const cell = createCell(cellSize);
+			cell.setAttribute("class", "cell");
 			grid.appendChild(cell);
 		}
 	}
 
-	document.querySelector("body").appendChild(grid);
+	return grid;
 }
